e25c33635d2c4f674b72a1279d7481e09ba477a7
msyfls123 <m18771058712@live.com>
2016-04-17 11:17:33 +0800

    Add GTK3

--- testpy/text.py
+++ testpy/text.py
@@ -1,8 +1,8 @@
 from math import *
 def makesin():
-    for i in range(360):
+    for i in range(90):
         yield sin(radians(i))
-k = "sinList=["+",".join(list(str(i) for i in makesin()))+"]"
+k = "["+",".join(list(str(i) for i in makesin()))+"]"
 file=open("a.txt","w")
 file.write(k)
 file.close()
