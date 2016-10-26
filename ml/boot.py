24ace245733df58508362f46232343da15b734cc
msyfls123 <m18771058712@live.com>
2016-10-26 22:50:15 +0800

    use PIL to show pic

--- ml/boot.py
+++ ml/boot.py
@@ -1,13 +1,18 @@
 import kNN
 import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from numpy import *
+from PIL import Image
 
 reload(kNN)
 datingDataMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
-# print datingDataMat,datingLabels[:20]
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.scatter(datingDataMat[:,1], datingDataMat[:,2], 15.0*array(datingLabels), 25.0*array(datingLabels))
-# ax.scatter(datingDataMat[:,1], datingDataMat[:,2], 15.0*array(datingLabels), [[0.9, 0.1, 0.9]])
-plt.show()
+plt.savefig('./myFig.jpg')
+# plt.show()
+
+im = Image.open('./myFig.jpg')
+im.show()
